3b4571e7a2c07c969e4180587fffc9cc22bd714c
Gbell26 <107581827+Gbell26@users.noreply.github.com>
2023-03-30 22:07:41 -0400

    1. Ball bounce! 2. Detect collision with Paddle 3. Detect when Ball is out of bounds 4. Keep score
    DONE!!!!

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from Scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,6 +13,10 @@ screen.tracer(0)
 r_paddle = Paddle(350,0)
 l_paddle = Paddle(-350,0)
 ball = Ball()
+l_score=Scoreboard(-75, 310)
+r_score = Scoreboard(75, 310)
+
+
 
 
 screen.listen()
@@ -22,11 +27,27 @@ screen.onkey(l_paddle.move_down, 's')
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
-    if ball.ycor() > 580 or ball.ycor() < -580:
-        ball.change_direction()
+
+
+    if ball.ycor() > 310 or ball.ycor() < -310:
+        ball.bounce_y()
+
+    # detect collision with right paddle
+
+    if ball.distance(r_paddle)<50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+    if ball.xcor()>340:
+        l_score.scored()
+        ball.start_right()
+
+    if ball.xcor()<-340:
+        r_score.scored()
+        ball.start_left()
+
+
 
 
 
